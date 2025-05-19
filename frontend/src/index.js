a50bf7f316d7ea25be63fc686480888c8a289a30
Ayush95697 <143510712+Ayush95697@users.noreply.github.com>
2025-05-19 23:06:00 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
-import ResumeChecker from './ResumeChecker';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ResumeChecker />
+    <App />
   </React.StrictMode>
 );
